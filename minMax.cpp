074cee1f1858d26613153635d15180ccfad7edcb
SwarnabhR <swarnabh.roy@gmail.com>
2023-05-23 12:19:39 +0530

    New commit

---
+++ minMax.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+    int arr[n];
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+    int max = 0, min = INT32_MAX;
+    for (int i = 0; i < n; i++)
+    {
+        if (arr[i] > max)
+        {
+            max = arr[i];
+        }
+        if (arr[i] < min)
+        {
+            min = arr[i];
+        }
+    }
+    cout << "Minimum: " << min << endl;
+    cout << "Maximum: " << max << endl;
+}
