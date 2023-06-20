588e03c5b79af3dd1177ce74a801ed945cba673c
SwarnabhR <swarnabh.roy@gmail.com>
2023-06-20 18:04:05 +0530

    new commit

---
+++ DNF.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+using namespace std;
+
+int DNF(int *arr, int n)
+{
+    int low = 0, mid = 0, high = n - 1;
+    while (mid <= high)
+    {
+        if (arr[mid] == 0)
+        {
+            swap(arr[mid], arr[low]);
+            mid++;
+            low++;
+        }
+        else if (arr[mid] == 1)
+        {
+            mid++;
+        }
+        else if (arr[mid] == 2)
+        {
+            swap(arr[high], arr[mid]);
+            high--;
+        }
+    }
+}
+
+int main()
+{
+
+    int n;
+    cin >> n;
+    int arr[n];
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+    DNF(arr, n);
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+
+    return 0;
+}
